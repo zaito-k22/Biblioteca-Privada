1686944f79d672c118f79042e56806e34180059b
Felipe Osorio Restrepo <felipe578av@gmail.com>
2025-03-11 13:45:15 -0500

    Agrege clase libro

---
+++ Biblioteca-Privada/Libro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Libro
+{
+
+    private int ID;
+    private string Titulo;
+    private string Autor;
+    private int precio;
+    private int stock;
+
+    public Libro(int ID, string Titulo, string Autor, int precio, int stock) { 
+        this.ID = ID;
+        this.Titulo = Titulo;
+        this.Autor = Autor;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+
+
+
+    }
+
